8329be69e792cb3e23aac69241120ddaf7729aa5
pasteltogrunon <juanfernandezesteban@gmail.com>
2026-09-18 01:35:17 +0200

    Turning back up the displacement multiplier for tall plants

--- src/main/java/net/junedev/viridium/client/renderers/blocks/TallPlantBlockRenderer.java
+++ src/main/java/net/junedev/viridium/client/renderers/blocks/TallPlantBlockRenderer.java
@@ -10,7 +10,7 @@ import net.minecraft.world.IBlockAccess;
 
 public class TallPlantBlockRenderer implements ISimpleBlockRenderingHandler {
 
-    private static final double RANDOM_OFFSET = 0.3D;
+    private static final double RANDOM_OFFSET = 0.4D;
 
     private final int renderId;
 
